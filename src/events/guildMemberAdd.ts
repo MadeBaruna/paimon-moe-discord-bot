31ac439d647e7591154948733a1138adacfa6a0f
I Made Setia Baruna <setia.baruna@sprintasia.co.id>
2021-08-28 15:24:43 +0700

    Welcome message only on paimon.moe server

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,7 +1,9 @@
 import { GuildMember } from 'discord.js';
 
 export async function onGuildMemberAdd(member: GuildMember): Promise<void> {
-  member.guild.systemChannel?.send(
-    `Welcome to paimon.moe, ${member.toString()}! Please read the <#820650147800809482> and <#829340541393043466> first, Thanks!`,
-  );
+  if (member.guild.id === '820601523125747712') {
+    member.guild.systemChannel?.send(
+      `Welcome to paimon.moe, ${member.toString()}! Please read the <#820650147800809482> and <#829340541393043466> first, Thanks!`,
+    );
+  }
 }
